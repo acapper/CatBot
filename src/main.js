81727e65616c7cb478247dd0bc41e7754e1ac186
Antony Capper <acapper56@gmail.com>
2019-02-05 07:47:05 +0000

    Added status. Added event handlers for joining and leaving guilds. Added rate limit console log.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ const commands = require('./commands');
 const config = require('../config');
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}`);
+	console.log(`Connect to ${client.guilds.array().length} guild(s)`);
+	updateStatus();
 });
 
 client.on('message', msg => {
@@ -13,4 +15,30 @@ client.on('message', msg => {
 	commands.all(msg, args);
 });
 
+client.on('guildCreate', guild => {
+	updateStatus();
+	console.log(`Joined a new guild: ${guild.name}`);
+});
+
+client.on('guildDelete', guild => {
+	updateStatus();
+	console.log(`Left a guild: ${guild.name}`);
+});
+
+client.on('rateLimit', limit => {
+	console.log(JSON.stringify(limit));
+});
+
+function updateStatus() {
+	client.user.setPresence({
+		afk: true,
+		game: {
+			name: `with ${client.guilds.array().length} guild(s)`,
+			type: 'PLAYING'
+		}
+	});
+}
+
 client.login(config.discord.botkey);
+
+// https://discordapp.com/oauth2/authorize?&client_id=281182988388007938&scope=bot&permissions=0
